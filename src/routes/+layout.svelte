02b5d588aca5dbecd1a6eeeb198609fea7f2a715
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-07-22 12:47:46 -0700

    Game init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,9 +5,7 @@
 
 <div class="app">
 	<Header />
-	<h1 class="text-3xl font-bold underline text-orange-500">
-		Hello world!
-	</h1>
+
 	<main>
 		<slot />
 	</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+
 </script>
 
 <svelte:head>
@@ -9,51 +7,11 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="flex items-center justify-center min-h-screen ">
+    <div class="grid grid-cols-3 w-full max-w-md border border-black">
+        {#each Array(9) as _, i}
+            <div class="flex items-center justify-center bg-gray-400 text-xl hover:bg-gray-700 h-48 border border-black" id="${i + 1}">
+            </div>
+        {/each}
+    </div>
+</div>
